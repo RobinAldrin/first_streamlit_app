18d69907983b7538046672a6ff0163223947c91a
RobinAldrin <106313795+RobinAldrin@users.noreply.github.com>
2022-05-27 13:23:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
-streamlit.title('my Parents new healthy diner')
+streamlit.title('My Parents new healthy diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
